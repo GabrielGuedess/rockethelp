1da5bf9b55eee64d450757917ef4511f5b672dc9
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-07-26 14:08:02 -0300

    Estrutura visual e Navegação

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {
 } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider, StatusBar } from 'native-base';
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
@@ -16,12 +16,12 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
+        barStyle='light-content'
+        backgroundColor='transparent'
         translucent={true}
       />
-  
-      {fontsLoaded ? <SignIn /> : <Loading />}
+
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
